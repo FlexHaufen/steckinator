1ca1c68555c76b529891022cb8da334aca263f36
flexhaufen <ibims1k0tt@gmail.com>
2025-11-12 19:18:59 +0100

    Added uart

---
+++ src/Uart/Uart.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file Uart.cpp
+ * @author flexhaufen
+ * @brief Implementation of Uart
+ * @version 0.1
+ * @date 2025-11-12
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+// *** INCLUDES ***
+#include "Uart/Uart.h"
+
+// *** PUPLIC FUNCTIONS ***
+
+Uart::Uart(uart_inst_t *uart, uint pinTx, uint pinRx, uint baudrate)
+    : m_uart(uart), m_pinTx(pinTx), m_pinRx(pinRx), m_baudrate(baudrate) {}
+
+void Uart::begin() {
+    uart_init(m_uart, m_baudrate);
+    gpio_set_function(m_pinTx, GPIO_FUNC_UART);
+    gpio_set_function(m_pinRx, GPIO_FUNC_UART);
+    uart_set_hw_flow(m_uart, false, false);
+    uart_set_format(m_uart, 8, 1, UART_PARITY_NONE);
+    uart_set_fifo_enabled(m_uart, false);
+}
+
+void Uart::writeLine(const std::string &s) {
+    uart_puts(m_uart, s.c_str());
+    uart_puts(m_uart, "\r\n");
+}
+
+int Uart::available() {
+    return uart_is_readable(m_uart);
+}
+
+std::string Uart::readLine() {
+    std::string result;
+
+    while (true) {
+        if (uart_is_readable(m_uart)) {
+            char c = uart_getc(m_uart);
+            if (c == '\n') {
+                break;
+            } else if (c != '\r') { // ignore carriage return
+                result += c;
+            }
+        }
+    }
+    return result;
+}
+
+void Uart::flush() {
+    // Wait until transmit buffer is empty
+    while (!uart_is_writable(m_uart))
+        tight_loop_contents();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,35 @@
  * 
  */
 
+
+// *** INCLUDES ***
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "Uart/Uart.h"
 
+// *** DEFINES ***
 #define GPIO_DEBUG_LED          25
 
 
-
+/**
+ * @brief Main
+ * 
+ */
 int main() {
-    stdio_init_all();
 
+    stdio_init_all();
 
     gpio_init(GPIO_DEBUG_LED);
     gpio_set_dir(GPIO_DEBUG_LED, GPIO_OUT);
     gpio_put(GPIO_DEBUG_LED, true);
 
-
+    // I dont know why the f**k but uart 1 is not working
+    Uart uart(uart1, 4, 5 ,9600);
+    uart.begin();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        uart.writeLine(uart.readLine());
     }
 
     return 0;
