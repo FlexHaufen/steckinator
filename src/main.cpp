053cf0d05ee38c998401f450726d07a34ebef58e
flexhaufen <ibims1k0tt@gmail.com>
2026-03-05 14:31:58 +0100

    Added USB logger

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,8 @@
 
 
 // *** INCLUDES ***
-#include <stdio.h>
 #include "pico/stdlib.h"
-
-#include "Uart/Uart.h"
+#include "Log/Log.h"
 
 // *** DEFINES ***
 #define GPIO_DEBUG_LED          25
@@ -25,19 +23,23 @@
  * 
  */
 int main() {
-
     stdio_init_all();
+
+
+    LOG_WAIT_FOR_USB;
+    LOG_INFO("Initializing");
+
 
     gpio_init(GPIO_DEBUG_LED);
     gpio_set_dir(GPIO_DEBUG_LED, GPIO_OUT);
     gpio_put(GPIO_DEBUG_LED, true);
 
-    // I dont know why the f**k but uart0 is not working
-    Uart uart(uart1, 4, 5 ,9600);
-    uart.begin();
+
 
     while (true) {
-        uart.writeLine(uart.readLine());
+
+
+        sleep_ms(1000);
     }
 
     return 0;
