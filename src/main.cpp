22ae150fb8e4f6b72c22919a98fdbab8c0172204
FlexHaufen <ibims1k0tt@gmail.com>
2026-03-12 12:50:31 +0100

    Added main configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,6 @@
 #include "pico/stdlib.h"
 #include "Log/Log.h"
 
-#include "GCodeParser/GCodeParser.h"
-
-// *** DEFINES ***
-#define GPIO_DEBUG_LED          25
-
 
 /**
  * @brief Main
@@ -28,25 +23,15 @@ int main() {
     stdio_init_all();
 
 
-    LOG_WAIT_FOR_USB;
-    LOG_INFO("Initializing");
-
-    // Debug
-    gpio_init(GPIO_DEBUG_LED);
-    gpio_set_dir(GPIO_DEBUG_LED, GPIO_OUT);
-    gpio_put(GPIO_DEBUG_LED, true);
+    //LOG_WAIT_FOR_USB;
+    //LOG_INFO("Initializing");
 
 
-    Steckinator::GCodeParser parser;
 
     while (true) {
 
-        auto event = parser.ParseLine("G01 X100 Y200 Z3.5 F20");
-        LOG_INFO("%f", event.x);
 
-
-        sleep_ms(2000);
-    }
+   }
 
     return 0;
 }
