8a8277da82e6ed68df07dcbcc13b5cc36b26d13a
flexhaufen <ibims1k0tt@gmail.com>
2026-03-22 14:28:49 +0100

    Created entrypoint, added namespace

--- src/main.cpp
+++ src/main.cpp
@@ -11,45 +11,17 @@
 
 
 // *** INCLUDES ***
-#include <pico/stdlib.h>
-#include "Log/Log.h"
-
-#include "Driver/TMC2209Driver.h"
-#include "Config.h"
+#include "Steckinator/Steckinator.h"
 
 /**
  * @brief Main
  * 
  */
 int main() {
-    stdio_init_all();
 
-
-    //LOG_WAIT_FOR_USB;
-    //LOG_INFO("Initializing");
-
-
-    TMC2209Driver m_0({
-        GPIO_M0_STEP,
-        GPIO_M0_DIR,
-        GPIO_M0_DIAG,
-        GPIO_M_EN,
-        1
-    });
-
-    //TMC2209Driver m_1({
-    //    GPIO_M1_STEP,
-    //    GPIO_M1_DIR,
-    //    GPIO_M1_DIAG,
-    //    GPIO_M_EN,
-    //    1
-    //});
-
-
-    m_0.setPosition(10);
-
-    while (true){}
+    Steckinator::Steckinator* steckinator = new Steckinator::Steckinator();
+    steckinator->Run();
+    delete steckinator;
     
-
     return 0;
 }
